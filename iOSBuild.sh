f911aca9f923840ba0dc2f2a74c1196295979c44
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-12-18 21:13:13 +0000

    Updated build script

--- iOSBuild.sh
+++ iOSBuild.sh
@@ -4,22 +4,34 @@ scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Script Path: ${scriptPath}"
 
 cd ${scriptPath}
+libraryString=""
 
-for build in iOS iOSSimulator; do
+builds=( iphoneos iphonesimulator macOS )
+libraries=( libOpenID libOpenIDC )
+for build in ${builds[@]}; do
 
-echo "Building for ${build}..."
-cmake --preset ${build}Build
-cmake --build --preset ${build}Build
+	echo "Building for ${build}..."
+	cmake --preset ${build}Build
+	cmake --build --preset ${build}Build
 
-for library in libOpenID libOpenIDC; do
-if [ ! -e "out/build/${build}Build/${library}.a" ]; then 
-    echo "Library ${library}.a not found"
-    exit 1;
-fi
+	for library in ${libraries[@]}; do
+		if [ "${build}" == "macOS" ]; then
+			releaseDir="Release"
+		else
+			releaseDir="Release-${build}"
+		fi
+		if [ ! -e "out/build/${build}Build/${releaseDir}/${library}.a" ]; then 
+    			echo "Library ${library}.a not found"
+    			exit 1;
+		fi
+
+		echo "Library saved to: out/${build}Build/${releaseDir}/${library}.a"
+	done
+
+	libraryString="${libraryString} -library out/build/${build}Build/${releaseDir}/\${library}.a"
 done
 
-done
-
 for library in libOpenID libOpenIDC; do
-    xcodebuild -create-xcframework -library out/build/iOSBuild/${library}.a -library out/build/iOSSimulatorBuild/${library}.a -output out/build/${library}.xcframework
+	libString=$(eval echo ${libraryString})
+    	xcodebuild -create-xcframework ${libString} -output out/build/${library}.xcframework
 done
