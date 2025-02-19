f924b3f7e0f018912be10813069bbd2f7559a9b8
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-02-19 15:23:55 +0000

    Added docs

---
+++ docs/conf.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+html_theme = "sphinx_rtd_theme"
+
+breathe_projects = {
+    "OpenID": "_build/xml/",
+}
+
+# Breathe configuration
+breathe_default_project = "OpenID"
+
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "OpenID"
+copyright = "2025, Z M Williams"
+author = "Z M Williams"
+
+extensions = [
+    "breathe",
+    "sphinx.ext.duration",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "myst_parser"
+]
+
+autodoc_typehints = 'description'
+
+autodoc_member_order = "bysource"
+
+napoleon_google_docstring = True
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+html_theme = "sphinx_rtd_theme"
+
+html_context = {
+    "display_github": True,
+    "github_user": "zwill22",
+    "github_repo": "iOSBuild",
+    "conf_py_path": "/docs/",
+    "github_version": "main"
+}
+
+autosectionlabel_prefix_document = True
---
+++ docs/index.rst
@@ -0,0 +1,34 @@
+.. OpenID documentation master file, created by
+   sphinx-quickstart on Wed Feb 19 14:44:52 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to OpenID's documentation!
+==================================
+
+This page contains the documentation for the `OpenID` project.
+
+.. _iOSBuild: https://github.com/zwill22/iOSBuild
+
+.. include:: ../README.md
+   :parser: myst_parser.sphinx_
+
+.. toctree:: 
+   :maxdepth: 2
+   :caption: Contents:
+   
+   about
+   modules
+   link_license
+
+.. seealso::
+
+   :doc: `contents`
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
